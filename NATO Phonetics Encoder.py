679208f629b9a09b6ce462bf632005eff09c14ca
Dr.M Dev <ms.dental.business@gmail.com>
2026-04-24 16:22:11 +0300

    logo implementation

--- NATO Phonetics Encoder.py
+++ NATO Phonetics Encoder.py
@@ -5,6 +5,9 @@ import pandas
 from gevent.util import print_run_info
 #_______________|
 from tkinter import *
+#_______________|
+MAIN_FONT = ("Courier", 11, "bold")
+BUTTONS_FONT = ("Times New Roma", 8, "bold")
 
 
 #========================Data-Base SETUP
@@ -33,9 +36,12 @@ print(final_NATO_ALPAHBET_DIC)
 # dimensions
 main_window_width = 600
 main_window_height = 600
-#
+#----
+icon_x = main_window_width / 4 - 20
+icon_y = main_window_height / 4 - 150
+#----
 widgets_x = main_window_width / 4 - 20
-widgets_y = main_window_height / 4
+widgets_y = main_window_height / 4 + 165
 
 #------------
 window = Tk()
@@ -45,15 +51,31 @@ window.maxsize(main_window_width,main_window_height)
 window.config(padx=20,pady=20)
 
 #_____________________________ICON
-canvas = Canvas(width=main_window_width/2, height=main_window_height/2, background="black")
-canvas.place(x=widgets_x,y=widgets_y-150)
+main_canvas = Canvas(width=main_window_width/2, height=main_window_height/2)
+#
+icon_image = PhotoImage(file=r"images/NATO Phonetics.png")
+prog_pic = main_canvas.create_image(300/2,300/2,image = icon_image)
+#
+main_canvas.place(x=icon_x,y=icon_y)
 
 #_____________________________INPUT
-input_label = Label(text="Enter Text Here")
+input_label = Label(text="Enter Text Here:", font=MAIN_FONT)
 input_label.place(x=widgets_x,y=widgets_y)
 
-#_____________________________OUTPUT
+input_entry = Entry(width=60)
+input_entry.place(x=widgets_x-30,y=widgets_y+25)
 
+
+#_____________________________OUTPUT
+output_y_displacement = 50
+####
+input_label = Label(text="Translation to NATO Phonetics:", font=MAIN_FONT)
+input_label.place(x=widgets_x,y=widgets_y
+                                +output_y_displacement)
+
+input_entry = Text(width=45,height=8)
+input_entry.place(x=widgets_x-30,y=widgets_y+25
+                                +output_y_displacement)
 
 
 
